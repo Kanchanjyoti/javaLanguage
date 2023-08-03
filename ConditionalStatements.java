b87fb37edf5f00fd2bcf6afbe44977d4671d23d1
kanchan <jyotikanchan973@gmail.com>
2023-08-03 15:54:40 +0530

    here is my another file date 3 august 2023

---
+++ ConditionalStatements.java
@@ -0,0 +1,73 @@
+import java.util.*;
+public class ConditionalStatements{
+    public static void main(String args[]){
+     Scanner sc = new Scanner(System.in);
+    //  int income= sc.nextInt();
+
+    //  int A= sc.nextInt();
+    //  int B= sc.nextInt();
+    //  int C= sc.nextInt();
+    // int marks= sc.nextInt();
+    int number = sc.nextInt();
+
+        // Print the largest of 2 
+
+     /*if(A>B){
+        System.out.print("A is largest of 2");
+     }
+     else{
+        System.out.print("B is largest of 2");*/
+
+    // ***************8****************8*****************8************
+    //  Check whether number is even or odd
+   /* if ((A%2) == 0){
+        System.out.print("The number is even");
+
+    }
+    else{
+        System.out.print("The number is odd");
+
+    }*/
+    // ***************8****************8*****************8************
+
+    // INcome tax problem
+
+      /*if(income <=500000){
+        System.out.print("NO Tax");
+      }
+      else if(income>500000 && income <1000000){
+        System.out.print("The tax will be "+ income*0.2);
+      }
+      else{
+        System.out.print("The tax will be "+ income*0.3);
+      }
+*/
+    // ***************8****************8*****************8************
+    // greatest of three numbers
+
+    /*
+    if(A>B && A> C){
+        System.out.print(A +" is greater");
+
+    }
+    else if(B>C){
+        System.out.print(B +" is greater");
+
+    }
+    else{
+        System.out.print(C+" is greater");
+    }
+    */
+    // ***************8****************8*****************8************
+
+// ternary operator
+//1. Pass or Fail
+
+// String outcome = (marks>33)? "Pass":"Fail";
+// System.out.print("The student is "+ outcome);
+
+//  2. odd or even 
+// String status = (number %2 == 0)? "even": "odd";
+//      System.out.print("The number is "+ status );
+     }
+    }
