bfa2174830fa307f27d7b54a9c2c24a48a06d985
kanchan <jyotikanchan973@gmail.com>
2023-08-03 06:43:31 +0530

    starting the class here

---
+++ Variables_Datatypes.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Variables_Datatypes{
+    public static void main(String args[]){
+
+        /*
+        System.out.print("hello world \n");   // \n 
+        System.out.println("hello world");    // println
+        System.out.print("hello world \n");
+
+        // simply making a pattern
+        System.out.println("* * * * ");
+        System.out.println("* * * ");
+        System.out.println("* *  ");
+        System.out.println("* ");      
+
+        // sum of a and b 
+         
+         int a , b , sum ;
+         a= 20;
+         b=10;
+         sum = a+b;
+         System.out.println( sum );
+
+         // Input in java
+         Scanner sc = new Scanner(System.in);
+         String word= sc.nextLine();
+         System.out.print(word);
+        //  int integer= sc.nextInt();
+        //  Byte smallInt= sc.nextByte();
+        //  float decimal = sc.nextFloat();
+        //  Boolean bigDecimal = sc.nextBoolean(); 
+
+
+        // Sum of two numbers
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print(a+b);    
+
+
+        // product of two numbers
+
+        Scanner sc= new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        System.out.print(a*b);  */
+
+
+        // Area of Circle
+
+        Scanner sc= new Scanner(System.in);
+        float radius= sc.nextFloat();
+        double area = 3.14 *radius*radius;
+        System.out.print(area);
+
+    }
+}
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,4 @@
+ int integer= sc.nextInt();
+        //  Byte smallInt= sc.nextByte();
+        //  float decimal = sc.nextFloat();
+        //  Boolean bigDecimal = sc.nextBoolean();
