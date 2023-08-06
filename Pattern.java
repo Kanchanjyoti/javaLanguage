0fd7713853087640ee9591cfd5df93c974d6e953
kanchan <jyotikanchan973@gmail.com>
2023-08-06 13:15:40 +0530

    this was the lesson

---
+++ Pattern.java
@@ -0,0 +1,45 @@
+public class Pattern{
+    public static void main(String args[]){
+        // Problem 1. print star pattern
+
+/*        for(int i=1; i <5; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(" * ");
+            }
+
+            System.out.println();
+        }                          */
+
+        // Problem 2. Print inverted star pattern 
+         
+ /*       int n=4;
+        for(int i=1;i<=n; i++){
+           for(int j=1; j<= n+1-i; j++ ){
+            System.out.print(" * ");
+           }
+           System.out.println();       
+
+        }
+         */
+
+        // Problem 3. Print half pyramid
+
+ /*       for(int i= 1; i<5; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print( j +" " );
+            }
+            System.out.println();
+        }                              */
+
+        // Problem 4. Print character pattern 
+
+ /*       char charats= 'A';
+        for(int i= 1; i<=4; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(charats + " ");
+                charats++; 
+            }
+            System.out.println();
+        }                           */
+    }
+}
