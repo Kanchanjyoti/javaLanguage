d066f4ebfe06d6b66a886ec42144981d0f576753
kanchan <jyotikanchan973@gmail.com>
2023-10-13 11:42:50 +0530

    new questions added

--- Arraylist.java
+++ Arraylist.java
@@ -1,6 +1,26 @@
 import java.util.ArrayList;
+import java.util.Collections;
 public class Arraylist{
-    public static void main(String[] args){
+    public static void swap(ArrayList<Integer> list, int i, int j){
+        int temp = list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, temp);
+    }
+
+    // Solution 5.
+    public static int storeWater(ArrayList<Integer> height ){
+     int maxWater = 0;
+     for(int i=0; i<height.size(); i++){
+        for(int j=i+1; j<height.size(); j++){
+            int ht= Math.min(height.get(i), height.get(j));
+            int width= j-i;
+            int currWater= ht* width;
+            maxWater= Math.max(maxWater, currWater);
+        }
+     }
+     return maxWater;
+
+}    public static void main(String[] args){
            // Syntex: ArrayList<DataType_Name> variable_Name = new ArrayList<>();
            ArrayList<Integer> list = new ArrayList<>();
            list.add(1);
@@ -21,12 +41,59 @@ public class Arraylist{
 
         }
         System.out.println();
+
         //  Problem 2. Find maximum in the arraylist
         int max= Integer.MIN_VALUE;
         for(int j=0; j<list.size();j++){
             max= Math.max(list.get(j), max);
         }
         System.out.println("Max No: " + max);
+
+        // Problem 3. Swap Two Numbers
+       swap(list, 1,2);
+       System.out.println(list);
+
+        //  Problem 4. Sorting an arraylist
+       Collections.sort(list);
+       System.out.println(list);
+       Collections.sort(list, Collections.reverseOrder());
+       System.out.println(list);
+
+    //     # Multidimentional ArrayList : 
+    ArrayList<ArrayList<Integer>> mainList= new ArrayList<>();
+    ArrayList<Integer> list1= new ArrayList<>();
+    ArrayList<Integer> list2= new ArrayList<>();
+    ArrayList<Integer> list3= new ArrayList<>();
+    list1.add(1);
+    list1.add(2);
+    list1.add(3);
+    list2.add(4);
+    list2.add(5);
+    list2.add(6);
+    list3.add(7);
+    list3.add(8);
+    list3.add(9);
+
+    mainList.add(list1);
+    mainList.add(list2);
+    mainList.add(list3);
+System.out.println(mainList);
+
+// Problem 5. Container with most water
+//  Brute force : Calculate all pairs and print the max
+   ArrayList<Integer> height= new ArrayList<>();
+   height.add(1);  
+   height.add(8);  
+   height.add(6);  
+   height.add(2);  
+   height.add(5);  
+   height.add(4);  
+   height.add(8);  
+   height.add(3);  
+   height.add(7);  
+
+System.out.println("The maximum water stored between two heights is "+ storeWater(height));
+
 
     }
     }
